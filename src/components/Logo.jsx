700bd9ad62773d1f6df0ffa75a5ce78834b5ecb3
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-11-14 20:10:37 -0300

    Created the logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components'
+import logo from '../assets/logo.svg';
+
+
+const Logo = (props) => {
+
+
+    return (
+        <div> 
+            <LogoStyle>
+                <a href="/">
+                    <LogoImg src={logo} alt="Logo" /> 
+                </a>
+                <a href="/">
+                    <LogoText>WADMIN</LogoText> 
+                </a>
+            </LogoStyle>
+        </div>
+    )
+}
+
+export default Logo;
+
+const LogoStyle = styled.div({
+    display:'flex',
+    marginInline:'10rem'
+
+})
+
+const LogoImg = styled.img({
+     width: '3.8rem',
+     marginTop:'1rem',
+})
+
+const LogoText = styled.p({
+    marginTop:'2rem',
+    marginBottom:'1.7rem',
+    fontSize: '1.5rem',
+    fontWeight:'700',
+    color:'#1E94EB'
+})

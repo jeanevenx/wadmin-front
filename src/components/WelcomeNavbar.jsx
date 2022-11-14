26ad960e73ed0a352d6ce9198da30cc3c946f5f0
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-11-14 20:11:57 -0300

    Created the welcomenavbar component

---
+++ src/components/WelcomeNavbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components'
+import '../styles/Welcome.css';
+import Logo from './Logo';
+
+
+const WelcomeNavbar = () => {
+
+
+    return (
+        <div>
+            <NavbarStyle>  
+                <Logo />
+                <MenuWelcome>
+                    <a href="/login" className="welcome-menu"><li>Entrar</li></a>
+                    <a href="/register" className="welcome-menu"
+                    ><li>Cadastre-se</li></a>
+                </MenuWelcome> 
+            </NavbarStyle>
+        </div>
+    )
+}
+
+export default WelcomeNavbar;
+
+const NavbarStyle = styled.nav({
+    width:'100%',
+    backgroundColor:'#FFF',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    boxShadow: 'rgba(99, 99, 99, 0.2) 0px 1px 2px 0px'
+
+})
+
+const MenuWelcome = styled.ul({
+    display:'flex',
+    justifyContent: 'space-between',
+    alignContent: 'space-around',
+    listStyleType: 'none',
+    marginRight:'10rem',
+    fontSize: '1.6rem'
+
+})
